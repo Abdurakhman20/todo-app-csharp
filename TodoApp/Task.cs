93d332a6dbb1f8af36a86519cfc6f59057d30dd9
Абдурахман Усманов <ausmanov706@gmail.com>
2024-07-08 15:27:29 +0300

    feat: Task class

---
+++ TodoApp/Task.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title {  get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Task(int id, string title, string description) 
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            CreatedAt = DateTime.Now;
+            IsCompleted = false;
+        }
+
+        public void ToggleTaskStatus()
+        {
+            bool status = IsCompleted;
+            IsCompleted = !status;
+        }
+
+        public override string ToString()
+        {
+            return $"[{(IsCompleted ? "X" : " ")}] {Id}: {Description}";
+        }
+    }
+}
